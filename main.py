49454f6e642954adc98d3dae10fe384f2aae4f1c
Chris Cozort <chriscozort@gmail.com>
2022-11-17 12:15:08 -0800

    added core files

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+overview of the thing 
+
+'''
+
+# sources: 
+
+# import libraries
+
+# built in
+
+# installed modules or libraries
+
+# created modules or libraries
+import settings
+import sprites
+# global variables
+
+# utilit
